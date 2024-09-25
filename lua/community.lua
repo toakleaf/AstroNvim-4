108d1b2373d46448cfb85e9f6f0274a11a5820a2
Tyler Oakleaf <tyleroakleaf@gmail.com>
2024-09-25 14:02:41 +0000

    More configuration

--- lua/community.lua
+++ lua/community.lua
@@ -8,6 +8,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.colorscheme.gruvbox-nvim" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.html-css" },
--- lua/plugins/overridden.lua
+++ lua/plugins/overridden.lua
@@ -3,7 +3,7 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
       filesystem = {
-        filtered_items = { hide_dotfiles = false },
+        filtered_items = { hide_dotfiles = false, hide_gitignored = false },
       },
     },
   },
